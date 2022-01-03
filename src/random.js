dd74ce58aa70878ee7b0de639954e5c999b04836
Ibrahim Ibrahim <ibrahimibrahim@gsd.harvard.edu>
2022-01-03 15:41:29 -0500

    test10

--- src/random.js
+++ src/random.js
@@ -1,6 +1,6 @@
 const myDiv1 = document.querySelector("#mydiv1");
 const randomLeftValue1 = Number.parseInt(Math.random() * (700) + 300);
-const randomTopValue1 =  Number.parseInt(Math.random() * (500) + 30);
+const randomTopValue1 = Number.parseInt(Math.random() * (500) + 30);
 myDiv1.style.left = `${randomLeftValue1}px`;
 myDiv1.style.top = `${randomTopValue1}px`;
 
@@ -9,7 +9,7 @@ myDiv1.style.top = `${randomTopValue1}px`;
 
 const myDiv2 = document.querySelector("#mydiv2");
 const randomLeftValue2 = Number.parseInt(Math.random() * 600+ 300);
-const randomTopValue2 =  Number.parseInt(Math.random() * 200 + 50);
+const randomTopValue2 = Number.parseInt(Math.random() * 200 + 50);
 myDiv2.style.left = `${randomLeftValue2}px`;
 myDiv2.style.top = `${randomTopValue2}px`;
 
@@ -17,15 +17,13 @@ myDiv2.style.top = `${randomTopValue2}px`;
 
 const myDiv3 = document.querySelector("#mydiv3");
 const randomLeftValue3 = Number.parseInt(Math.random() * (800) + 200);
-const randomTopValue3 =  Number.parseInt(Math.random() * (500) + 10);
+const randomTopValue3 = Number.parseInt(Math.random() * (500) + 10);
 myDiv3.style.left = `${randomLeftValue3}px`;
 myDiv3.style.top = `${randomTopValue3}px`;
 
 
-
 const myDiv4 = document.querySelector("#mydiv4");
 const randomLeftValue4 = Number.parseInt(Math.random() * (1000) + 200);
-const randomTopValue4 =  Number.parseInt(Math.random() * (500) + 40);
+const randomTopValue4 = Number.parseInt(Math.random() * (500) + 40);
 myDiv4.style.left = `${randomLeftValue4}px`;
 myDiv4.style.top = `${randomTopValue4}px`;
-
