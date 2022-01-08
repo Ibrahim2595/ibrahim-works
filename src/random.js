a6b73364414ffb03b9b7bde9168e6165c34b4612
Ibrahim Ibrahim <ibrahimibrahim@gsd.harvard.edu>
2022-01-08 03:24:43 -0500

    test12

--- src/random.js
+++ src/random.js
@@ -1,29 +1,31 @@
+// GEO JUNGLE
 const myDiv1 = document.querySelector("#mydiv1");
-const randomLeftValue1 = Number.parseInt(Math.random() * (700) + 300);
-const randomTopValue1 = Number.parseInt(Math.random() * (500) + 30);
+const randomLeftValue1 = Number.parseInt(Math.random() * (570) + 600);
+const randomTopValue1 = Number.parseInt(Math.random() * (350) + 50);
 myDiv1.style.left = `${randomLeftValue1}px`;
 myDiv1.style.top = `${randomTopValue1}px`;
 
 
 
-
+// ROCKBOT
 const myDiv2 = document.querySelector("#mydiv2");
-const randomLeftValue2 = Number.parseInt(Math.random() * 600+ 300);
+const randomLeftValue2 = Number.parseInt(Math.random() * 470+ 425);
 const randomTopValue2 = Number.parseInt(Math.random() * 200 + 50);
 myDiv2.style.left = `${randomLeftValue2}px`;
 myDiv2.style.top = `${randomTopValue2}px`;
 
 
-
+// PAINTERLY
 const myDiv3 = document.querySelector("#mydiv3");
-const randomLeftValue3 = Number.parseInt(Math.random() * (800) + 200);
-const randomTopValue3 = Number.parseInt(Math.random() * (500) + 10);
+const randomLeftValue3 = Number.parseInt(Math.random() * (770) + 425);
+const randomTopValue3 = Number.parseInt(Math.random() * (500) + 50);
 myDiv3.style.left = `${randomLeftValue3}px`;
 myDiv3.style.top = `${randomTopValue3}px`;
 
 
+// APORETIC FUTURES
 const myDiv4 = document.querySelector("#mydiv4");
-const randomLeftValue4 = Number.parseInt(Math.random() * (1000) + 200);
-const randomTopValue4 = Number.parseInt(Math.random() * (250) + 10);
+const randomLeftValue4 = Number.parseInt(Math.random() * (770) + 425);
+const randomTopValue4 = Number.parseInt(Math.random() * (150) + 50);
 myDiv4.style.left = `${randomLeftValue4}px`;
 myDiv4.style.top = `${randomTopValue4}px`;
